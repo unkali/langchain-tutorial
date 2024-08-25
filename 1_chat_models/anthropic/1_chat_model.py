64853aaee1e7364884511a8ea7cde5119c8a8f78
Kapil Mangtani <40874971+unkali@users.noreply.github.com>
2024-08-25 18:59:23 +0530

    anthropic

---
+++ 1_chat_models/anthropic/1_chat_model.py
@@ -0,0 +1,30 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+
+# Load environment variables from .env
+load_dotenv()
+
+llm = ChatAnthropic(
+    model="claude-3-5-sonnet-20240620",
+    temperature=0,
+    max_tokens=1024,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+result = llm.invoke(messages)
+print("Full result:")
+print(result)
+print("Content only:")
+print(result.content)
